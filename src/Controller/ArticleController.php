99a2f00608194e8976309868843c74044b04d447
htunhtunhtet <ryanhhh91@gmail.com>
2018-11-05 18:50:08 +0800

    add services

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,9 +3,12 @@
 	namespace App\Controller;
 	
 	
+	use Psr\Log\LoggerInterface;
 	use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 	use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+	use Symfony\Component\HttpFoundation\JsonResponse;
 	use Symfony\Component\HttpFoundation\Response;
+	use Twig\Environment;
 	
 	class ArticleController extends AbstractController
 	{
@@ -21,7 +24,7 @@
 		/**
 		 * @Route("/news/{slug}", name="article_show")
 		 */
-		public function show($slug)
+		public function show($slug, Environment $twigEnvironment)
 		{
 			
 			$comments = [
@@ -34,9 +37,24 @@
 			
 			//dump($slug,$this);
 			
-			return $this->render('article/show.html.twig', [
+			$html = $twigEnvironment->render('article/show.html.twig', [
 				'title' => ucwords(str_replace('-',' ',$slug)),
-				'comments' =>$comments
+				'comments' =>$comments,
+				'slug'  =>$slug
 			]);
+			
+			return new Response($html);
+			
+			
+		}
+		
+		/**
+		 * @Route("/news/{slug}/heart", name="article_toggle_heart" ,methods={"POST"})
+		 */
+		public function toggleArticleHeart($slug, LoggerInterface $logger)
+		{
+			
+			$logger->info('Hearted');
+			return new JsonResponse(['hearts'=>rand(5,100)]);
 		}
 	}
